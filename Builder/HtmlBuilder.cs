26b10cd494a5bf9572deee5131d7b67715f71bc2
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-05-19 11:47:04 -0700

    separate the file and change to fluent interface

---
+++ Builder/HtmlBuilder.cs
@@ -0,0 +1,31 @@
+namespace Builder
+{
+        public class HtmlBuilder
+        {
+            private readonly string rootName;
+            HtmlElement root = new HtmlElement();
+            public HtmlBuilder(string rootName)
+            {
+                this.rootName = rootName;
+                root.Name = rootName;
+            }
+
+            public HtmlBuilder AddChild(string childName, string childText)
+            {
+                // Fluent Interface
+                var e = new HtmlElement(childName, childText);
+                root.Elements.Add(e);
+                return this;
+            }
+
+            public override string ToString()
+            {
+                return root.ToString();
+            }
+
+            public void Clear()
+            {
+                root = new HtmlElement{Name = rootName};
+            }
+        }
+}
--- Builder/Program.cs
+++ Builder/Program.cs
@@ -4,74 +4,6 @@ using System.Text;
 
 namespace Builder
 {
-        public class HtmlElement
-        {
-            public string Name, Text;
-            public List<HtmlElement> Elements = new List<HtmlElement>(); 
-            private const int indentSize = 2;
-
-            public HtmlElement()
-            {
-
-            }
-            public HtmlElement(string name, string text)
-            {
-                Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
-                Text = text ?? throw new ArgumentNullException(paramName: nameof(text));
-            }    
-
-            private string ToStringImpl(int indent)
-            {
-                var sb = new StringBuilder();
-                var i = new string(' ', indentSize * indent);
-                sb.AppendLine($"{i}<{Name}>");
-
-                if (!string.IsNullOrWhiteSpace(Text))
-                {
-                    sb.Append(new string(' ', indentSize * (indent + 1)));
-                    sb.AppendLine(Text);
-                }
-
-                foreach (var e in Elements)
-                {
-                    sb.Append(e.ToStringImpl(indent + 1));
-                }
-                sb.AppendLine($"{i}</{Name}>\n");
-                return sb.ToString();
-            }
-
-            public override string ToString()
-            {
-                return ToStringImpl(0);
-            }
-        }
-
-        public class HtmlBuilder
-        {
-            private readonly string rootName;
-            HtmlElement root = new HtmlElement();
-            public HtmlBuilder(string rootName)
-            {
-                this.rootName = rootName;
-                root.Name = rootName;
-            }
-
-            public void AddChild(string childName, string childText)
-            {
-                var e = new HtmlElement(childName, childText);
-                root.Elements.Add(e);
-            }
-
-            public override string ToString()
-            {
-                return root.ToString();
-            }
-
-            public void Clear()
-            {
-                root = new HtmlElement{Name = rootName};
-            }
-        }
     class Program
     {
         static void Main(string[] args)
@@ -97,8 +29,8 @@ namespace Builder
 
 
             var builder = new HtmlBuilder("ul");
-            builder.AddChild("li", "hello");
-            builder.AddChild("li", "world");
+
+            builder.AddChild("li", "hello").AddChild("li", "world");
             Console.WriteLine(builder.ToString());
         }
     }
